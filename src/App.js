cda74c13753f6cb8b1ad519d6888ac628fa87327
foxik-nd <froxik77430@outlook.fr>
2024-07-04 09:54:04 +0200

    components maps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LocationForm from './components/LocationForm';
 import LocationList from './components/LocationList';
+import MapView from './components/MapView';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    const fetchLocations = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/locations`);
+        const data = await response.json();
+        setLocations(data);
+      } catch (error) {
+        console.error('Error retrieving locations:', error);
+      }
+    };
+    fetchLocations();
+  }, []);
+
   return (
     <div className="App">
       <h1>TRAVEL APP</h1>
       <LocationForm />
       <LocationList />
+      <MapView locations={locations} />
     </div>
   );
 }
---
+++ src/components/MapView.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+const MapView = ({ locations = [] }) => {
+  const customIcon = new L.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
+
+  return (
+    <MapContainer
+      style={{ height: '400px', width: '100%' }}
+      center={[48.8566, 2.3522]}
+      zoom={13}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {locations.length > 0 && locations.map((location) => (
+        <Marker
+          key={location._id}
+          position={[location.latitude, location.longitude]}
+          icon={customIcon}
+        >
+          <Popup>
+            Latitude: {location.latitude}, Longitude: {location.longitude}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
+
+export default MapView;
